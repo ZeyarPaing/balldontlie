88e8bde1ed4722994e1e571ba7e76f7c07900a00
Zeyar Paing <zeyarpaing@ucsy.edu.mm>
2022-06-12 15:21:09 +0630

    fix: do not perform fetch on rerender

--- store/actions/postAction.js
+++
@@ -1,8 +0,0 @@
-import * as types from "../types";
-
-export const fetchPosts = () => async (dispatch) => {
-  dispatch({
-    type: types.GET_POSTS,
-    payload: ["1st post", "2nd post", "3rd post"],
-  });
-};
